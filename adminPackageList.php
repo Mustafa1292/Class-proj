68df257bdc3690b1dc4ddd45356578dd92fa4617
Mike Yannuzzi (University of Houston) <43522450+myannuzzi@users.noreply.github.com>
2022-04-15 21:59:45 -0500

    Addded pages for admin pages

---
+++ adminPackageList.php
@@ -0,0 +1,20 @@
+<?php
+    include_once 'includes/databaseConnect.php'
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Admin page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <?php
+            print "Pakcage list";
+        ?>
+        <input type="button" value="Say Hi!" onclick="location='test.php'" />
+    </body>
+</html>
---
+++ adminPage.php
@@ -0,0 +1,22 @@
+<?php
+    include_once 'includes/databaseConnect.php'
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Admin page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <?php
+            print "Admin choices";
+        ?>
+        <input type="button" value="See user list!" onclick="location='adminUserList.php'" />
+        <input type="button" value="See package list!" onclick="location='adminPackageList.php'" />
+        <input type="button" value="Logout!" onclick="location='test.php'" />
+    </body>
+</html>
