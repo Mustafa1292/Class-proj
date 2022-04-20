26d42fbcd58638904666b01708c9f9264dc055cf
dcarlon00 <78058905+dcarlon00@users.noreply.github.com>
2022-04-20 17:37:09 -0500

    Add files via upload

---
+++ Forms/delete.php
@@ -0,0 +1,13 @@
+<?php
+include 'connnect.php';
+if(isset($_GET['deleteid'])){
+    $id=$_GET['deleteid'];
+
+    $sql="delete from `users` where id=$id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+       header('location:dashboard.php');
+    }
+}
+
+?>
