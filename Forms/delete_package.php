ee16911a56e514d645dffe46c6cf96e1a638649a
dcarlon00 <78058905+dcarlon00@users.noreply.github.com>
2022-04-21 00:20:32 -0500

    Add files via upload

---
+++ Forms/delete_package.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'connnect.php';
+
+if(isset($_GET['deleteid'])) {
+    $id=$_GET['deleteid'];
+
+    $sql="delete from `parcel` where packageID=$id";
+    $result=mysqli_query($con, $sql);
+    if($result){
+        header('location:dashboard.php');
+    } else {
+        die(mysqli_error($con));
+    }
+}
+
+?>
---
+++ Forms/logout.php
@@ -0,0 +1,35 @@
+<?php 
+include 'connnect.php';
+
+session_destroy();
+ echo "<h3>Come again! Click below to return home or login.</h3>";
+
+ 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../stylesheets/styles.css" />
+    <link rel="stylesheet" href="styles2.css" />
+ <body>
+<div><nav id="nav">
+      <div class="navTop">
+        <div class="navItem">
+          <a href="../index.html">
+            <h1>
+              <span style="font-size: 36px; letter-spacing: 10px"
+                ><em> UH Post Office </em></span
+              >
+            </h1>
+            <h1><span style="font-size: 20px; letter-spacing: 5px"
+                ><em>Logout</em></h1>
+          </a>
+        </div>
+      </div>
+    </nav>
+        <button type=submit name="submit" class="btn btn-primary"><a href="login.php">return</a></button>
+      </div>
+</body>
+</html>
