362f8f2ba44f63033bc07a8001eb5b42c9b75c3b
Mike Yannuzzi (University of Houston) <43522450+myannuzzi@users.noreply.github.com>
2022-04-21 16:05:45 -0500

    added chart to page

--- Forms/reports.php
+++ Forms/reports.php
@@ -100,7 +100,7 @@ echo "<h3>For our last quarter we had "," $calc total users and $packs total pac
 </table>
 
 <canvas id="myChart" width="400" height="400"></canvas>
-<script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.bundle.min.js">
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar',
