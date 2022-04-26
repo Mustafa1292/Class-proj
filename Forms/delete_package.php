32e590374b48830c07465bef9762eb690af66ce5
Mustafa1292 <35036281+Mustafa1292@users.noreply.github.com>
2022-04-26 13:53:53 -0500

    Updated

--- Forms/delete_package.php
+++ Forms/delete_package.php
@@ -4,14 +4,17 @@ include 'connnect.php';
 
 if(isset($_GET['deleteid'])) {
     $id=$_GET['deleteid'];
+    //$resultt=mysqli_query($con,$sqll);
+//echo "$uname";
+
+ 
 
     $sql="delete from `parcel` where packageID=$id";
     $result=mysqli_query($con, $sql);
     if($result){
-        header('location:dashboard.php');
+            header('location:dashboard.php');
     } else {
         die(mysqli_error($con));
     }
 }
-
 ?>
